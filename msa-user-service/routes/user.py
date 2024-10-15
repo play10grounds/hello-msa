44b8c17d434fdf9e549cfaa0ba241778dc80246f
play10grounds <play10grounds@gmail.com>
2024-10-15 17:15:40 +0900

    user service - UserList 스키마, 회원 목록 조회 기능 추가

--- msa-user-service/routes/user.py
+++ msa-user-service/routes/user.py
@@ -1,10 +1,12 @@
+from typing import List
+
 from fastapi import APIRouter
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 
-from schema.user import User, UserBase
+from schema.user import User, UserBase, UserList
 from service.database import get_db
-from service.user import register
+from service.user import register, userlist
 
 router = APIRouter()
 
@@ -13,3 +15,12 @@ async def new_user(user: UserBase, db: Session=Depends(get_db)):
     print(user)
 
     return register(db, user)
+
+@router.get('/users', response_model=list[UserList])
+async def list_users(db: Session=Depends(get_db)):
+    users = userlist(db)
+
+    # 테이블 조회한 결과 객체를
+    # UserList 형식의 배열로 재생성
+    #return [UserList.from_orm(u) for u in users]
+    return [UserList.model_validate(u) for u in users]
--- msa-user-service/service/user.py
+++ msa-user-service/service/user.py
@@ -1,7 +1,11 @@
+from typing import List
+
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from models.user import User
-from schema.user import UserBase
+from schema.user import UserBase, UserList
+
 
 # 회원가입 처리
 # 기본 회원정보 + 번호,가입일
@@ -13,3 +17,8 @@ def register(db: Session, user: UserBase):
     print(user)
 
     return user
+
+# 회원 목록 조회
+def userlist(db: Session):
+    return db.query(User.mno, User.userid,
+            User.name, User.regdate).all()
