c07246d40b8c512d5c9d121ecd8ebb4f6c3f1b53
play10grounds <play10grounds@gmail.com>
2024-10-18 14:57:07 +0900

    frontend - axios, session 패키지 설치, 서버측 세션 기능 추가, 세션여부에 따라 secure 페이지 접근 제어, 로그아웃 기능 추가

--- msa-frontend/public/js/userlogin.js
+++ msa-frontend/public/js/userlogin.js
@@ -1,5 +1,6 @@
 const loginbtn = document.querySelector('#loginbtn');
 const loginfrm = document.loginfrm;
+const naver = document.querySelector('#naver');
 
 loginbtn.addEventListener('click', async () => {
     const formData = new FormData(loginfrm);
@@ -38,3 +39,19 @@ loginbtn.addEventListener('click', async () => {
         });
 
 });
+
+// 네이버 로그인 버튼 표시
+// 즉시 실행 함수 : 일반적인 함수 실행은 함수 정의후 호출시 이루어 짐
+// 하지만, 즉시 실행 함수는 함수 정의후 바로 함수를 실행하게 함
+// (함수명() { 함수몸체 })(), (() => { 함수몸체 })()
+(() => {
+    // naver.innerHTML = "<p>Hello, World!!</p>";
+    fetch('http://127.0.0.1:3000/api/naver2')
+        .then(res => {
+            if (res.status === 200) {
+                return res.text();
+            }
+        }).then(data => {
+            naver.innerHTML = data;
+        })
+})();
